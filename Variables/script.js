3bcac6dd410d379a1316c9939236afc00d39dfcd
Mahek Sheth <maheksheth77@gmail.com>
2025-09-16 18:50:23 +0530

    Variables

---
+++ Variables/script.js
@@ -0,0 +1,27 @@
+console.log("Variables")
+
+//Var --> Global
+//Let -->Local 
+//Const -->Local + immutable
+
+//DataType varName=value; -->Strongly typed language
+//JS  --> Loosely typed language
+
+//Naming Conv.
+//fname
+//f_name
+//FName
+//fName (Preff) Camel Casing
+
+if(true){
+    let age=20;
+    var fname="Mahek";
+    const pi=3.14;
+    console.log(age);
+    pi=3;
+    console.log(pi);
+}
+// console.log(age);
+console.log(fname);
+
+
