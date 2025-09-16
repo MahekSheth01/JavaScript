80c0c9da02fc9f31440d0b4ef86c282964f62e5d
Mahek Sheth <maheksheth77@gmail.com>
2025-09-16 19:16:47 +0530

    DataTypes

---
+++ Datatypes-operators/script.js
@@ -0,0 +1,54 @@
+//Data Types
+
+//Numbers(int,float,decimal)
+
+let num=3.14;
+let num2=2;
+
+//String
+
+const fname="Mahek";
+const sname='Sheth';
+console.log(fname+" "+sname);
+
+//Booleans
+
+let isLoggedIn=true;
+console.log(isLoggedIn+10)//1+10=11
+//true-->1
+//false-->0
+
+//NULL
+
+let lastLoginDate=null;
+console.log(lastLoginDate)
+
+//Undefined
+
+let lastLoginTime;
+console.log(lastLoginTime)
+
+//Objects
+
+const person={
+    fname:"Mahek",
+    lname:"Sheth",
+    age:22,
+}
+console.log(person.fname)
+
+//Array
+
+let fruits=['Apple','Mango','Grapes',11]
+console.log(fruits)
+
+//Operators
+
+//string '1'+'1'-->string
+//string+number=string
+//string*number=number
+console.log('123'*1);//123
+console.log('a'*1);//NaN
+
+//Typeof operator
+console.log(typeof '123');
