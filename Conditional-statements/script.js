ebd9d63a3ba831a5410a97732dbc2f6b552b9d00
Mahek Sheth <maheksheth77@gmail.com>
2025-09-17 21:57:41 +0530

    Conditional Statements

---
+++ Conditional-statements/script.js
@@ -0,0 +1,31 @@
+const age=10
+//if else || else if ladder
+
+if (age==18){
+    console.log("Yess you can vote")
+}
+else if(age>60){
+    console.log("As you are senior citizen so you can vote")
+}
+else{
+    console.log("You are not eligible to vote")
+}
+
+//Ternary operator
+
+let result=age>=18 ? 'yes':'No'
+console.log(result)
+
+
+//Switch case
+const opt=0;
+switch(opt){
+    case 1:
+        console.log('One')
+        break;
+    case 2:
+        console.log('Second')
+        break;
+    default:
+        console.log('Invalid')
+}
