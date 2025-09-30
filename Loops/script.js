03da74b9529fe1bd725a77b646caa95a017fe395
Mahek Sheth <maheksheth77@gmail.com>
2025-09-30 11:40:23 +0530

    Loops

---
+++ Loops/script.js
@@ -0,0 +1,37 @@
+//loops
+//for loop-->If you know the number of iterations
+//while loop-->If you don't know the number of iterations
+//do while loop-->Executes at least once
+//for in loop-->iterates over the properties of an object
+//for of loop-->iterates over the values of an iterable object like array, string, map, set
+
+//for loop
+for(let i=0;i<5;i++){
+    console.log(i)
+}
+//while loop
+let j=0
+while(j<5){
+    console.log(j)
+    j++
+}
+//do while loop
+let k=0 
+do{
+    console.log(k)
+    k++
+}while(k<5)
+//for in loop
+const person={
+    name:'John',
+    age:30,
+    city:'New York'
+}   
+for(let key in person){
+    console.log(key,person[key])
+}
+//for of loop
+const colors=['red','green','blue']
+for(let color of colors){
+    console.log(color)
+}
