cae3b36d5d532ded0ded186ac6262cd6ff793743
Mahek Sheth <maheksheth77@gmail.com>
2025-10-05 17:41:01 +0530

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+//DOM-->Document Object Model
+alert("Hello World");
+const value=prompt("Enter your name");
+console.log("Hello : "+value);
+// console.log(window);
+//Document is a property of window object
+document.title="DOM Manipulation";
+document.write("<h1>Hello</h1><br>"+value);
