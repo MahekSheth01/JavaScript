4357515b93f4314be1de9b6bdc139c8198522229
Mahek Sheth <maheksheth77@gmail.com>
2025-11-03 18:27:03 +0530

    current location

--- current-location/script.js
+++ current-location/script.js
@@ -1,1 +1,23 @@
-alert('Greeting from script.js in current-location!');
+const button = document.getElementById('get-location-btn');
+
+async function getData(lat,long){
+    const promise =await fetch(`http://api.weatherapi.com/v1/current.json?key=d08716ecd87046cc9c954207250111&q=${lat},${long}&aqi=yes`)
+    return await promise.json();
+}
+
+async function getLocation(params) {
+    const res=await getData(params.coords.latitude,params.coords.longitude);
+    console.log(res);
+}
+
+function showPosition(position) {
+    // console.log(position);
+}
+
+function showError(error) {
+    console.log("error");
+}
+
+button.addEventListener('click', async() => {
+    const res=navigator.geolocation.getCurrentPosition(getLocation, showError);
+})
