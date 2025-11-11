b60b17eb7385298066f74aa96eb4b5feba9124dc
Mahek Sheth <maheksheth77@gmail.com>
2025-11-11 13:05:00 +0530

    Currying

--- Currying/script.js
+++ Currying/script.js
@@ -1,4 +1,4 @@
-alert('Hello, world!');
+alert('Currying in JavaScript');
 //Currying is a technique in functional programming where a function with multiple arguments is transformed into a sequence of functions, each taking a single argument. This allows for partial application of functions, enabling the creation of specialized functions from more general ones.
 function curry(func) {
     return function curried(...args) {
@@ -31,4 +31,5 @@ console.log(curriedMultiply(2, 3)(4)); // Outputs: 24
 console.log(curriedMultiply(2)(3, 4)); // Outputs: 24
 
 // Currying can be particularly useful in scenarios where functions need to be reused with some arguments fixed, enhancing code modularity and readability.
+
 
