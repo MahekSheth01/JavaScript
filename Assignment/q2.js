486a7b8901fbcec2fe1a60e984b4fa655d1c2e46
Mahek Sheth <maheksheth77@gmail.com>
2026-01-23 18:51:02 +0530

    Palindrome

---
+++ Assignment/q2.js
@@ -0,0 +1,15 @@
+//Write a program to check palindrome number.
+
+const num=121
+
+let temp=num
+let sum=0;
+
+while(temp>0){
+    let num=temp%10;
+    sum=sum*10+num
+    temp=Math.floor(temp/10)
+}
+if(sum===num){
+    console.log(num+" is a Palindrome number");
+}
